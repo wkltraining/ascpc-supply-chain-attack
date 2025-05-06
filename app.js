b8a7383f40c21b5731e1ff4c11bfa8741dcd3f65
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2025-05-06 19:56:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 80
-const Example = require('@wkl-training/ascpc-npm-example');
+// const Example = require('@wkl-training/ascpc-npm-example');
 
-const example = new Example();
+// const example = new Example();
 
 process.env.SECRET_KEY = '1234567890';
 process.env.DB_USER = 'ascpc-fake-user';
 process.env.DB_PASSWORD = 'ascpc-fake-password';
 
+
+// app.get('/', (req, res) => {
+//     res.send(example.sayHello())
+//   })
+
 app.get('/', (req, res) => {
-  res.send(example.sayHello())
+  res.send("Hello")
 })
 
 app.listen(port, () => {
